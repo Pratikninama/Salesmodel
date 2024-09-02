b99bd88cbf2a7d9262fea929fabca75f911e6c68
Praik <Pratikninama.outlook.com>
2024-09-02 16:24:06 +0530

    2nd commit

--- sales_transform.py
+++ sales_transform.py
@@ -6,7 +6,6 @@ State_dataframes= pd.read_excel(r'C:\Users\prati\PycharmProjects\Salesmodel\samp
 #State_dataframes['state_code'] = Sales_dataframe['State'].str[:3]
 Sales_dataframe['Price Per Item'] = Sales_dataframe['Sales']/Sales_dataframe['Quantity']
 print(Sales_dataframe)
-#Sales_dataframe.to_csv(r'C:\Pratik WHIZAI.csv', index=False)
 new_df = pd.merge(Sales_dataframe,State_dataframes,on='State',how="left")
 print(new_df)
 new_df.to_csv(r'C:\Users\prati\PycharmProjects\Salesmodel\output_data\Sales_ouput.csv', index=False)
