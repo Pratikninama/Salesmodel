ae33f5bc2dc8fc0bee8013fe8356328b2ac75328
Praik <Pratikninama.outlook.com>
2024-09-09 12:18:06 +0530

    5th commit

--- sales_transform.py
+++ sales_transform.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import xlrd
 import xlrd.sheet
-Sales_dataframe= pd.read_excel(r'C:\Users\prati\PycharmProjects\Salesmodel\sample_data\Sample - Superstore.xls')
-State_dataframes= pd.read_excel(r'C:\Users\prati\PycharmProjects\Salesmodel\sample_data\abberrevation.xls')
+Sales_dataframe= pd.read_excel(r'sample_data\Sample - Superstore.xls')
+State_dataframes= pd.read_excel(r'sample_data\abberrevation.xls')
 #State_dataframes['state_code'] = Sales_dataframe['State'].str[:3]
 Sales_dataframe['Price Per Item'] = Sales_dataframe['Sales']/Sales_dataframe['Quantity']
 print(Sales_dataframe)
 new_df = pd.merge(Sales_dataframe,State_dataframes,on='State',how="left")
 print(new_df)
-new_df.to_csv(r'C:\Users\prati\PycharmProjects\Salesmodel\output_data\Sales_output.csv', index=False)
+new_df.to_csv(r'output_data\Sales_output.csv', index=True)
+Sales_dataframe['Shipment Days']= Sales_dataframe['Ship Date']-Sales_dataframe['Order Date']
+print(Sales_dataframe)
+new_df = pd.merge(Sales_dataframe,new_df,on='State',how="left")
+new_df.to_csv(r'output_data\Sales_output.csv', index=True)
