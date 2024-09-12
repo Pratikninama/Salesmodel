4fe6dcc593bb98e8bc9ce9a92027312b0975e614
Praik <Pratikninama.outlook.com>
2024-09-12 10:59:57 +0530

    Sales DAG Edit

--- Sales_dag.py
+++ Sales_dag.py
@@ -1,8 +1,8 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-import xlrd
 import pandas as pd
 from datetime import datetime, timedelta
+import os
 
 default_args = {
     'owner': 'airflow',
@@ -16,30 +16,39 @@ dag = DAG(
     'read_transform_load_csv',
     default_args=default_args,
     description='A DAG to read, transform, and load CSV data',
-    schedule='0 9 * * *',  # Set to 'daily' or any other interval as needed
+    schedule_interval='0 9 * * *',  # Set to 'daily' or any other interval as needed
 )
 
 
 def read_excel(**kwargs):
-    excel_file_path = pd.read_excel(r'sample_data\Sample - Superstore.xls')
-    df = pd.read_excel(excel_file_path, **kwargs)
-    kwargs['ti'].xcom_push(key='raw_data', value=df.to_dict())
+    file_path = r'sample_data/Sample - Superstore.xls'
+    df = pd.read_excel(file_path)
+    # Serialize DataFrame to JSON for XCom
+    kwargs['ti'].xcom_push(key='raw_data', value=df.to_json())
 
 
 def transform_data(**kwargs):
     ti = kwargs['ti']
+    # Pull and deserialize DataFrame from XCom
     raw_data = ti.xcom_pull(key='raw_data', task_ids='read_excel')
-    df = pd.DataFrame.from_dict(raw_data)
+    df = pd.read_json(raw_data)
 
-    df['transformed_column'] = df['Sales'] /df['Quantity']
-    ti.xcom_push(key='transformed_data', value=df.to_dict())
+    # Perform transformation
+    df['transformed_column'] = df['Sales'] / df['Quantity']
+    # Serialize transformed DataFrame to JSON for XCom
+    ti.xcom_push(key='transformed_data', value=df.to_json())
+
 
 def load_to_csv(**kwargs):
     ti = kwargs['ti']
+    # Pull and deserialize DataFrame from XCom
     transformed_data = ti.xcom_pull(key='transformed_data', task_ids='transform_data')
-    df = pd.DataFrame.from_dict(transformed_data)
+    df = pd.read_json(transformed_data)
+
     output_csv_file_path = r'output_data/Sales_DAG.csv'
-    df.to_csv('output_data/Sales_DAG.csv', index=False)
+    # Ensure output directory exists
+    os.makedirs(os.path.dirname(output_csv_file_path), exist_ok=True)
+    df.to_csv(output_csv_file_path, index=False)
 
 
 # Define tasks
@@ -64,5 +73,6 @@ load_to_csv_task = PythonOperator(
     dag=dag,
 )
 
+# Set task dependencies
 read_excel_task >> transform_data_task >> load_to_csv_task
-#impport sales
+
